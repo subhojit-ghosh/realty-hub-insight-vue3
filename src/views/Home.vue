673973a6603ca524a720dfe8e007da333e044def
etam168 <etam168@gmail.com>
2022-01-06 12:25:48 +0800

    testing on the footer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Home Page</h1>
+  <h1>Home Page test</h1>
 </template>
 
 <script lang="ts">
--- src/views/layout/app-footer.vue
+++ src/views/layout/app-footer.vue
@@ -1,13 +1,34 @@
 <template>
   <q-footer class="bg-primary text-white">
-    <q-toolbar> </q-toolbar>
+    <q-card dark flat class="bg-primary">
+      <q-card-section class="q-my-xs text-center">
+        <q-avatar v-for="icon in icons" :key="icon" class="q-ma-sm" icon="home">
+        </q-avatar>
+      </q-card-section>
+      <q-separator color="white" inset />
+      <q-card-section class="text-center">
+        <p>
+          Insight Property Consultants (Statement of Particulars of Business
+          Number: E-301073-A000).<br />
+
+          ©2020 All rights reserved.<br />
+        </p>
+      </q-card-section>
+    </q-card>
   </q-footer>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "app-footer",
+  components: {},
+  setup() {
+    const icons = ref(["mdi-facebook", "twitter", "linkedin", "instagram"]);
+    return {
+      icons,
+    };
+  },
 });
 </script>
